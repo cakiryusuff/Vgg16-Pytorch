7d66cbd82a777d7c5c225229b3189d798416c5c0
cakiryusuff <cakir_yusuff@outlook.com>
2023-08-25 23:34:14 +0300

    Update vgg16.py

--- vgg16.py
+++ vgg16.py
@@ -35,7 +35,7 @@ class Vgg16(nn.Module):
 
     def layer(self, in_planes, out_planes):
       blocks = nn.Sequential(
-          nn.Conv2d(in_planes, out_planes, kernel_size = 3, padding = 1),
+          nn.Conv2d(in_planes, out_planes, kernel_size = 3, padding = 1, bias = False),
           nn.ReLU(inplace = True),
           nn.BatchNorm2d(out_planes))
       return blocks
